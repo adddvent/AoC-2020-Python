3691fb59b95ddfbedc0dce9cc3b5303b0819ec28
adddvent <45513082+adddvent@users.noreply.github.com>
2020-12-22 16:10:12 +0100

    Update 12.py
    reduced duplicated code

--- src/12.py
+++ src/12.py
@@ -12,52 +12,49 @@ F11
 """.strip() # 25 ; 286
 
 
+cmd_param_all = [(s[0], int(s[1:])) for s in ss.splitlines()]
+
+
+def apply_trans(cmd, num_steps, x, y):
+    if cmd == 'N': y -= num_steps
+    elif cmd == 'E': x += num_steps
+    elif cmd == 'S': y += num_steps
+    elif cmd == 'W': x -= num_steps
+    return x, y
+
+
+def apply_rot(cmd, degrees, dx, dy):
+    num_rot90 = (degrees % 360) // 90
+    if cmd == 'L':
+        for _ in range(num_rot90):
+            dy, dx = -dx, dy
+    elif cmd == 'R':
+        for _ in range(num_rot90):
+            dy, dx = dx, -dy
+    return dx, dy
+
+
 r, c = 0, 0
 dr, dc = 0, 1
-for s in ss.splitlines():
-    a, *aa = s
-    mydir = a
-    am = int(''.join(aa))
-    if mydir == 'N': r -= am
-    elif mydir == 'E': c += am
-    elif mydir == 'S': r += am
-    elif mydir == 'W': c -= am
-    elif mydir == 'L':
-        for _ in range((am % 360) // 90):
-            dr, dc = -dc, dr
-    elif mydir == 'R':
-        for _ in range((am % 360) // 90):
-            dr, dc = dc, -dr
-    elif mydir == 'F':
-        r, c = r + am * dr, c + am * dc
+for cmd, param in cmd_param_all:
+    c, r = apply_trans(cmd, param, c, r)
+    dc, dr = apply_rot(cmd, param, dc, dr)
+    if cmd == 'F':
+        r, c = r + param * dr, c + param * dc
 
 res_a = abs(r) + abs(c)
 
 print('solution a:', res_a)
 
 
-
 r, c = 0, 0
 dr, dc = 0, 1
-
 dy, dx = r - 1, c + 10
-
-for s in ss.splitlines():
-    a, *aa = s
-    mydir = a
-    am = int(''.join(aa))
-    if mydir == 'N': dy -= am
-    elif mydir == 'E': dx += am
-    elif mydir == 'S': dy += am
-    elif mydir == 'W': dx -= am
-    elif mydir == 'L':
-        for _ in range((am % 360) // 90):
-            dy, dx = -dx, dy
-    elif mydir == 'R':
-        for _ in range((am % 360) // 90):
-            dy, dx = dx, -dy
-    elif mydir == 'F':
-        r, c = r + am * dy, c + am * dx
+for cmd, param in cmd_param_all:
+    dx, dy = apply_trans(cmd, param, dx, dy)
+    dx, dy = apply_rot(cmd, param, dx, dy)
+    if cmd == 'F':
+        r, c = r + param * dy, c + param * dx
 
 res_b = abs(r) + abs(c)
 
